269790202bdd46739c56420da40a002b7f1e622c
Kexibt <44288584+Kexibt@users.noreply.github.com>
2019-09-20 01:09:23 +0300

    Create 771.Jewels and Stones.cpp

---
+++ 771.Jewels and Stones.cpp
@@ -0,0 +1,26 @@
+// ConsoleApplication2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <cmath>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+		setlocale(LC_ALL, "");
+		map <char, bool> isJ;
+		string J, S;
+		char c;
+		cin >> J;
+		for (int i = 0; i < J.size(); ++i) {
+			isJ[J[i]] = true;
+		}
+		int count = 0;
+		cin >> S;
+		for (int i = 0; i < S.size(); ++i) {
+			if (isJ[S[i]]) ++count;
+		}
+		cout << count;
+		return 0;
+}
