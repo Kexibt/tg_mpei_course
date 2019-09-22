b71cea2542dbc2b747f8bae7c71f024e3bc6d9d5
Kexibt <44288584+Kexibt@users.noreply.github.com>
2019-09-22 19:07:52 +0300

    Update 50.Pow(x,n).cpp

--- 50.Pow(x,n).cpp
+++ 50.Pow(x,n).cpp
@@ -1,18 +1,11 @@
 // ConsoleApplication4.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 // https://leetcode.com/problems/powx-n/
 
-#include <iostream>
-#include <cmath>
-using namespace std;
-
-int main(int argc, char* argv[])
-{
-	int n;
-	double x;
-	int count=0;
-	cin >> x >> n;
-
-	if (n == 0) return 1;
+class Solution {
+public:
+    double myPow(double x, int n) {
+    int count=0;
+    if (n == 0) return 1;
 	if (n == 1) return x;
 	if (x == 0 || x == 1) return x;
 	do {
@@ -25,4 +18,5 @@ int main(int argc, char* argv[])
 		x *= x;
 	}
 	return x;
-}
+    }
+};
