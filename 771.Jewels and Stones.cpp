e248ca1f9800e76bca7b83b0d472eb767dcfc199
Kexibt <44288584+Kexibt@users.noreply.github.com>
2019-09-22 19:05:27 +0300

    Update 771.Jewels and Stones.cpp

--- 771.Jewels and Stones.cpp
+++ 771.Jewels and Stones.cpp
@@ -1,27 +1,18 @@
 // ConsoleApplication2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 // https://leetcode.com/problems/jewels-and-stones/
 
-#include <iostream>
-#include <vector>
-#include <map>
-#include <cmath>
-using namespace std;
-
-int main(int argc, char* argv[])
-{
-		setlocale(LC_ALL, "");
+class Solution {
+public:
+    int numJewelsInStones(string J, string S) {
 		map <char, bool> isJ;
-		string J, S;
 		char c;
-		cin >> J;
 		for (int i = 0; i < J.size(); ++i) {
 			isJ[J[i]] = true;
 		}
 		int count = 0;
-		cin >> S;
 		for (int i = 0; i < S.size(); ++i) {
 			if (isJ[S[i]]) ++count;
 		}
-		cout << count;
-		return 0;
-}
+        return count;
+    };
+    };
